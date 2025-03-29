af690b10893716dd13cca8dcac4b10b687fe70f6
fuyutarow <fuyutarow@gmail.com>
2025-03-30 00:57:03 +0900

    Add SWR dependency and enhance Sui dApp structure
    - Introduced SWR version 2.3.3 for improved data fetching and caching.
    - Updated package.json to include SWR as a dependency.
    - Enhanced the counter detail page to display additional information such as counter ID and value.
    - Added new files and structures for Move call dependencies, improving the framework for on-chain interactions.
    - Implemented various utility functions and structures to streamline data management within the Sui dApp.

--- ui/src/app/counters/[counterId]/page.tsx
+++ ui/src/app/counters/[counterId]/page.tsx
@@ -2,6 +2,10 @@
 
 import { useParams } from 'next/navigation';
 import { AppBar } from '@/src/components/AppBar';
+import { useEffect, useState } from 'react';
+import { Counter } from '@/src/libs/moveCall/counter/counter/structs';
+import { suiClient } from '@/src/contracts';
+import useSWR from 'swr';
 
 type Params = {
   counterId: string;
@@ -9,11 +13,19 @@ type Params = {
 
 const Page = () => {
   const { counterId } = useParams<Params>();
+  const { data: counter } = useSWR(
+    counterId,
+    async (key: string) => await Counter.fetch(suiClient, key),
+    { revalidateOnFocus: false },
+  );
+
   return (
     <div className="min-h-screen flex flex-col">
       <AppBar />
       <main className="flex-grow flex flex-col items-center p-8">
-        <p>{counterId}/poap</p>
+        <p>{counterId}/counter</p>
+        <p>{counter?.id}</p>
+        <p>{counter?.value}</p>
       </main>
     </div>
   );
--- ui/src/libs/moveCall/_framework/init-loader.ts
+++ ui/src/libs/moveCall/_framework/init-loader.ts
@@ -1,7 +1,14 @@
 import * as package_onchain_1 from '../_dependencies/onchain/0x1/init';
 import * as package_onchain_2 from '../_dependencies/onchain/0x2/init';
+import * as package_source_1 from '../_dependencies/source/0x1/init';
 import * as package_onchain_1eeb55d09f4bed6e03df6ca25b70eccef7fa1c3cfa640e2679cd9ea8719bfaf0 from '../counter/init';
+import * as package_source_2 from '../sui/init';
 import { StructClassLoader } from './loader';
+
+function registerClassesSource(loader: StructClassLoader) {
+  package_source_1.registerClasses(loader);
+  package_source_2.registerClasses(loader);
+}
 
 function registerClassesOnchain(loader: StructClassLoader) {
   package_onchain_1.registerClasses(loader);
@@ -13,4 +20,5 @@ function registerClassesOnchain(loader: StructClassLoader) {
 
 export function registerClasses(loader: StructClassLoader) {
   registerClassesOnchain(loader);
+  registerClassesSource(loader);
 }
